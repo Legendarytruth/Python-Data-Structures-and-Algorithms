57678be28374c0bb48ab3466bba2214fcb7f4912
Legendarytruth <arthur.sit99@gmail.com>
2021-10-24 10:28:59 -0400

    4. Depth-First Search

--- Starting Files/dfs.py
+++ Starting Files/dfs.py
@@ -10,7 +10,22 @@ from stack import Stack
 
 
 def dfs(maze, start, goal):
-    pass
+    stack = Stack()
+    stack.push(start)
+    predecessors = {start: None}
+    
+    while not stack.is_empty():
+        current_cell = stack.pop()
+        if current_cell == goal:
+            return get_path(predecessors, start, goal)
+        for direction in ["up", "right", "down", "left"]:
+            row_offset, col_offset = offsets[direction]
+            neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset)
+            if is_legal_pos(maze, neighbour) and neighbour not in predecessors:
+                stack.push(neighbour)
+                predecessors[neighbour] = current_cell
+    return None
+
 
 
 if __name__ == "__main__":
@@ -22,7 +37,7 @@ if __name__ == "__main__":
     assert result == [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
 
     # Test 2
-    maze = read_maze("mazes/mini_maze_dfs.txt")
+    maze = read_maze("Starting Files\mazes\mini_maze_dfs.txt")
     for row in maze:
         print(row)
     start_pos = (0, 0)
@@ -31,7 +46,7 @@ if __name__ == "__main__":
     assert result == [(0, 0), (0, 1), (1, 1), (2, 1), (2, 2)]
 
     # Test 3
-    maze = read_maze("mazes/mini_maze_dfs.txt")
+    maze = read_maze("Starting Files\mazes\mini_maze_dfs.txt")
     start_pos = (0, 0)
     goal_pos = (3, 3)
     result = dfs(maze, start_pos, goal_pos)
