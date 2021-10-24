3091b15fac2514e4b6614e3277fd4eabe80056ef
Legendarytruth <arthur.sit99@gmail.com>
2021-10-24 10:08:34 -0400

    2. Stacks

--- Starting Files/reverse_string.py
+++ Starting Files/reverse_string.py
@@ -11,5 +11,8 @@ reversed_string = ""
 s = stack.Stack()
 
 # Your solution here.
-
+for i in string:
+    s.push(i)
+while(not s.is_empty()):
+    reversed_string += s.pop()
 print(reversed_string)
