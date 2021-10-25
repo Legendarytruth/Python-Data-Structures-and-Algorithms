672c0cc156af7fc32ca362615ceaf55fa7c085ac
Legendarytruth <arthur.sit99@gmail.com>
2021-10-24 23:16:41 -0400

    5. Queue

--- Starting Files/queue_ll.py
+++ Starting Files/queue_ll.py
@@ -8,4 +8,36 @@ from collections import deque
 
 
 class Queue:
-    pass
+    
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+    
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == "__main__":
+    q = Queue()
+    print(q)
+    print(q.is_empty())
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    print(q)
+    print(q.dequeue())
+    print(q.peek())
+    print(q.is_empty())
